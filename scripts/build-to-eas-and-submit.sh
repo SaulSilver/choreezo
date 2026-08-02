608688c659c7f448ec0b0efdd2c6edf28cec83f1
Hatem <hatem.houssein@gmail.com>
2026-08-02 17:24:01 +0200

    Prod build scripts and app icon

---
+++ scripts/build-to-eas-and-submit.sh
@@ -0,0 +1,2 @@
+eas env:push production --path .env.production
+eas build --platform ios --profile production --auto-submit
---
+++ scripts/rebuild.sh
@@ -0,0 +1,5 @@
+rm -rf ~/Library/Developer/Xcode/DerivedData/Choreezo-*
+cd /Users/hatemhoussein/projects/choreezo/ios
+pod install
+cd ..
+npx expo run:ios --device
